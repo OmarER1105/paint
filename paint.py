c57b57d35c22815eb31334e06c79d9a617429d9c
Omar Estrada <OmarE1105@hotmail.com>
2024-03-22 12:42:21 -0600

    Se añadio la funcion de triangulo y rectangulo

--- paint.py
+++ paint.py
@@ -43,12 +43,33 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count%2 == 0:
+            forward(end.x - start.x)
+            left(90)
+        else:
+            forward(end.x - start.x/2)
+            left(90)
+    end_fill()
+    pass  
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
+    pass 
 
 
 def tap(x, y):
